9ced894d5e55459f3398c7aa9b6bc5cf2b991708
shubhamperforce <shubham.bhadke@perforce.com>
2025-08-20 07:33:06 +0530

    updated module to support latest PE version.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,12 @@
-# == Class: xinetd::params
-#
-class xinetd::params {
-  $default_user   = 'root'
-  $package_ensure = 'installed'
+# modules/xinetd/manifests/params.pp
+class xinetd::params (
+  String $default_user  = 'root',
+  String $default_group = 'root',
+  String $package_ensure = 'installed',
+) {
 
-  case $facts['os']['family'] {
-    'FreeBSD': { $default_group = 'wheel' }
-    default: { $default_group = 'root' }
-  }
-
-  case $facts['os']['family'] {
-    'Debian':  {
+  case $::osfamily {
+    'Debian': {
       $confdir            = '/etc/xinetd.d'
       $conffile           = '/etc/xinetd.conf'
       $package_name       = 'xinetd'
@@ -18,19 +14,17 @@ class xinetd::params {
       $service_hasstatus  = false
       $service_name       = 'xinetd'
       $service_restart    = "/usr/sbin/service ${service_name} reload"
-      $service_status     = undef
     }
     'FreeBSD': {
       $confdir            = '/usr/local/etc/xinetd.d'
       $conffile           = '/usr/local/etc/xinetd.conf'
+      $default_group      = 'wheel'
       $package_name       = 'security/xinetd'
       $service_hasrestart = false
       $service_hasstatus  = true
       $service_name       = 'xinetd'
-      $service_restart    = undef
-      $service_status     = undef
     }
-    'Suse':  {
+    'Suse': {
       $confdir            = '/etc/xinetd.d'
       $conffile           = '/etc/xinetd.conf'
       $package_name       = 'xinetd'
@@ -38,9 +32,8 @@ class xinetd::params {
       $service_hasstatus  = false
       $service_name       = 'xinetd'
       $service_restart    = "/sbin/service ${service_name} reload"
-      $service_status     = undef
     }
-    'RedHat':  {
+    'RedHat': {
       $confdir            = '/etc/xinetd.d'
       $conffile           = '/etc/xinetd.conf'
       $package_name       = 'xinetd'
@@ -48,7 +41,6 @@ class xinetd::params {
       $service_hasstatus  = true
       $service_name       = 'xinetd'
       $service_restart    = "/sbin/service ${service_name} reload"
-      $service_status     = undef
     }
     'Gentoo': {
       $confdir            = '/etc/xinetd.d'
@@ -57,39 +49,22 @@ class xinetd::params {
       $service_hasrestart = true
       $service_hasstatus  = true
       $service_name       = 'xinetd'
-      $service_restart    = undef
-      $service_status     = undef
-    }
-    'Archlinux': {
-      $confdir            = '/etc/xinetd.d'
-      $conffile           = '/etc/xinetd.conf'
-      $package_name       = 'xinetd'
-      $service_hasrestart = true
-      $service_hasstatus  = true
-      $service_name       = 'xinetd'
-      $service_restart    = undef
-      $service_status     = undef
     }
     'Linux': {
-      case $facts['os']['name'] {
+      case $::operatingsystem {
         'Amazon': {
-          $confdir            = '/etc/xinetd.d'
-          $conffile           = '/etc/xinetd.conf'
-          $package_name       = 'xinetd'
-          $service_name       = 'xinetd'
-          $service_hasrestart = true
-          $service_hasstatus  = false
-          $service_restart    = undef
-          $service_status     = undef
+          $confdir      = '/etc/xinetd.d'
+          $conffile     = '/etc/xinetd.conf'
+          $package_name = 'xinetd'
+          $service_name = 'xinetd'
         }
         default: {
-          fail("xinetd: module does not support Linux operatingsystem ${facts['os']['name']}")
+          fail("xinetd: module does not support Linux operatingsystem ${::operatingsystem}")
         }
       }
     }
-    default:   {
-      fail("xinetd: module does not support osfamily ${facts['os']['family']}")
+    default: {
+      fail("xinetd: module does not support osfamily ${::osfamily}")
     }
   }
-
 }
